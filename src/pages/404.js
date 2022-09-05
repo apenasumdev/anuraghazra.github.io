4ce562d82d8736d02fae8f74115c5cecf0a0fd75
┼̷S̷̷o̷̷u̷̷z̷̷x̷̷1̷̷7̷┼ <109921888+souzx17@users.noreply.github.com>
2022-09-05 03:49:10 -0300

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -8,7 +8,7 @@ import Link from 'gatsby-link';
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>Opps, seems like you are lost!</h1>
+    <h1>Opps, seems like you are lostt!</h1>
     <p>
       Mind going back? or <Link to="/blog">read some blogs</Link>
     </p>
